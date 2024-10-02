4d71fa882032a422795df6164999d1876bc2af43
Sean Murphy <sean@iamseanmurphy.com>
2024-10-02 13:27:12 +0000

    1.0.4

---
+++ fluent-forms-connector-for-mailpoet/Integrations/Bootstrap.php
@@ -0,0 +1,305 @@
+<?php
+
+namespace FluentFormMailPoet\Integrations;
+
+use FluentForm\App\Http\Controllers\IntegrationManagerController;
+use FluentForm\Framework\Foundation\Application;
+use FluentForm\Framework\Helpers\ArrayHelper as Arr;
+
+class Bootstrap extends IntegrationManagerController
+{
+    public $hasGlobalMenu = false;
+
+    public $disableGlobalSettings = 'yes';
+
+    public function __construct(Application $app)
+    {
+        parent::__construct(
+            $app,
+            'MailPoet',
+            'mailpoet',
+            '_fluentform_mailpoet_settings',
+            'mailpoet_feeds',
+            99
+        );
+
+        $this->logo = FFMAILPOET_URL . 'assets/mailpoet.png';
+
+        $this->description = 'Connect MailPoet with WP Fluent Forms and subscribe a contact when a form is submitted.';
+
+        $this->registerAdminHooks();
+
+       // add_filter('fluentform/notifying_async_mailpoet', '__return_false');
+    }
+
+    public function pushIntegration($integrations, $formId)
+    {
+        $integrations[$this->integrationKey] = [
+            'title' => $this->title . ' Integration',
+            'logo' => $this->logo,
+            'is_active' => $this->isConfigured(),
+            'configure_title' => 'Configuration required!',
+            'global_configure_url' => '#',
+            'configure_message' => 'MailPoet is not configured yet! Please configure your MailPoet api first',
+            'configure_button_text' => 'Set MailPoet'
+        ];
+        return $integrations;
+    }
+
+    public function getIntegrationDefaults($settings, $formId)
+    {
+        return [
+            'name' => '',
+            'first_name' => '',
+            'last_name' => '',
+            'email' => '',
+            'other_fields' => [
+                [
+                    'label' => '',
+                    'item_value' => ''
+                ]
+            ],
+            'list_id' => '',
+            'send_confirmation_email' => false,
+            'conditionals' => [
+                'conditions' => [],
+                'status' => false,
+                'type' => 'all'
+            ],
+            'enabled' => true
+        ];
+    }
+
+    public function getSettingsFields($settings, $formId)
+    {
+        return [
+            'fields' => [
+                [
+                    'key' => 'name',
+                    'label' => 'Feed Name',
+                    'required' => true,
+                    'placeholder' => 'Your Feed Name',
+                    'component' => 'text'
+                ],
+                [
+                    'key' => 'list_id',
+                    'label' => 'MailPoet List',
+                    'placeholder' => 'Select MailPoet List',
+                    'tips' => 'Select the MailPoet List you would like to add your contacts to.',
+                    'component' => 'select',
+                    'required' => true,
+                    'options' => $this->getLists(),
+                ],
+                [
+                    'key' => 'CustomFields',
+                    'require_list' => false,
+                    'label' => 'Primary Fields',
+                    'tips' => 'Associate your MailPoet merge tags to the appropriate Fluent Form fields by selecting the appropriate form field from the list.',
+                    'component' => 'map_fields',
+                    'field_label_remote' => 'MailPoet Field',
+                    'field_label_local' => 'Form Field',
+                    'primary_fileds' => [
+                        [
+                            'key' => 'email',
+                            'label' => 'Email Address',
+                            'required' => true,
+                            'input_options' => 'emails'
+                        ],
+                        [
+                            'key' => 'first_name',
+                            'label' => 'First Name'
+                        ],
+                        [
+                            'key' => 'last_name',
+                            'label' => 'Last Name'
+                        ]
+                    ]
+                ],
+                [
+                    'key' => 'other_fields',
+                    'require_list' => false,
+                    'label' => 'Custom Fields',
+                    'tips' => 'Select which Fluent Form fields pair with their<br /> respective MailPoet fields.',
+                    'component' => 'dropdown_many_fields',
+                    'field_label_remote' => 'MailPoet Field',
+                    'field_label_local' => 'Form Field',
+                    'options' => $this->getCustomFields()
+                ],
+                [
+                    'key' => 'send_confirmation_email',
+                    'require_list' => false,
+                    'checkbox_label' => 'Send Confirmation Email',
+                    'component' => 'checkbox-single'
+                ],
+                [
+                    'require_list' => false,
+                    'key' => 'conditionals',
+                    'label' => 'Conditional Logics',
+                    'tips' => 'Allow MailPoet integration conditionally based on your submission values',
+                    'component' => 'conditional_block'
+                ],
+                [
+                    'require_list' => false,
+                    'key' => 'enabled',
+                    'label' => 'Status',
+                    'component' => 'checkbox-single',
+                    'checkbox_label' => 'Enable This feed'
+                ]
+            ],
+            'button_require_list' => false,
+            'integration_title' => $this->title
+        ];
+    }
+
+    public function getMergeFields($list, $listId, $formId)
+    {
+        return [];
+    }
+    
+    protected function getCustomFields()
+    {
+        $api = $this->getApi();
+        $customFields = $api->getSubscriberFields();
+        
+        $fields = [];
+        if (empty($customFields)) {
+            return $fields;
+        }
+        foreach ($customFields as $customField) {
+            $id = Arr::get($customField, 'id');
+            $name = Arr::get($customField, 'name');
+            if ($id && $name) {
+                $fields['cf_' . $customField['id']] = $customField['name'];
+            }
+        }
+        return $fields;
+    }
+
+
+    protected function getLists()
+    {
+        $api = $this->getApi();
+        $lists = $api->getLists();
+        $formattedLists = [];
+        foreach ($lists as $list) {
+            $formattedLists[$list['id']] = $list['name'];
+        }
+
+        return $formattedLists;
+    }
+
+
+    /*
+     * Form Submission Hooks Here
+     */
+    public function notify($feed, $formData, $entry, $form)
+    {
+        $data = $feed['processedValues'];
+        $contact = Arr::only($data, ['first_name', 'last_name', 'email']);
+
+        if (!is_email($contact['email'])) {
+            $contact['email'] = Arr::get($formData, $contact['email']);
+        }
+
+        foreach (Arr::get($data, 'other_fields') as $field) {
+            if ($field['item_value']) {
+                $contact[$field['label']] = $field['item_value'];
+            }
+        }
+
+        if (!is_email($contact['email'])) {
+            do_action_deprecated(
+                'ff_integration_action_result',
+                [
+                    $feed,
+                    'info',
+                    'MailPoet API called skipped because no valid email available'
+                ],
+                FLUENTFORM_FRAMEWORK_UPGRADE,
+                'fluentform/integration_action_result',
+                'Use fluentform/integration_action_result instead of ff_integration_action_result.'
+            );
+            do_action('fluentform/integration_action_result', $feed, 'info', 'MailPoet API called skipped because no valid email available');
+            return;
+        }
+
+
+        $api = $this->getApi();
+
+        try {
+            $subscriber = $api->getSubscriber($contact['email']);
+            if ($subscriber) {
+                do_action_deprecated(
+                    'ff_integration_action_result',
+                    [
+                        $feed,
+                        'info',
+                        'Contact creation has been skipped because contact already exist at MailPoet'
+                    ],
+                    FLUENTFORM_FRAMEWORK_UPGRADE,
+                    'fluentform/integration_action_result',
+                    'Use fluentform/integration_action_result instead of ff_integration_action_result.'
+                );
+                do_action('fluentform/integration_action_result', $feed, 'info', 'Contact creation has been skipped because contact already exist at MailPoet');
+                return;
+            }
+        } catch (\Exception $exception) {
+
+        }
+
+        try {
+            $options = [
+                'skip_subscriber_notification' => true,
+                'send_confirmation_email' => Arr::isTrue($data, 'send_confirmation_email')
+            ];
+
+            $subscriber = $api->addSubscriber($contact, [
+                Arr::get($data, 'list_id')
+            ], $options);
+
+            do_action_deprecated(
+                'ff_integration_action_result',
+                [
+                    $feed,
+                    'success',
+                    'Contact has been created in MailPoet. Contact ID: ' . $subscriber['id']
+                ],
+                FLUENTFORM_FRAMEWORK_UPGRADE,
+                'fluentform/integration_action_result',
+                'Use fluentform/integration_action_result instead of ff_integration_action_result.'
+            );
+
+            do_action('fluentform/integration_action_result', $feed, 'success', 'Contact has been created in MailPoet. Contact ID: ' . $subscriber['id']);
+        } catch (\Exception $exception) {
+            do_action_deprecated(
+                'ff_integration_action_result',
+                [
+                    $feed,
+                    'failed',
+                    $exception->getMessage()
+                ],
+                FLUENTFORM_FRAMEWORK_UPGRADE,
+                'fluentform/integration_action_result',
+                'Use fluentform/integration_action_result instead of ff_integration_action_result.'
+            );
+
+            do_action('fluentform/integration_action_result', $feed, 'failed', $exception->getMessage());
+        }
+    }
+
+    protected function getApi()
+    {
+        return \MailPoet\API\API::MP('v1');
+    }
+
+
+    public function isConfigured()
+    {
+        return true;
+    }
+
+    public function isEnabled()
+    {
+        return true;
+    }
+}
---
+++ fluent-forms-connector-for-mailpoet/readme.txt
@@ -0,0 +1,95 @@
+=== Fluent Forms Connector for MailPoet  ===
+Contributors: techjewel,wpmanageninja
+Tags: Integration, MailPoet, Form, Integration, Fluent Forms
+Requires at least: 5.0
+Tested up to: 6.5.2
+Requires PHP: 7.4
+Stable tag: 1.0.4
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Connect Fluent Forms with MailPoet.
+
+== Description ==
+Create MailPoet Newsletter subscribers from your Fluent Forms Form Submission.
+
+== Add-On Fluent Forms – PailPoet 3 ==
+<a href="https://wordpress.org/plugins/fluentform">Fluent Forms</a> is a free and fastest form builder plugin for WordPress. This Plugin integrates with MailPoet by providing an option for your users to signup in MailPoet email newsletter lists while submitting a form in WordPress.
+
+=== FEATURES ===
+- Allow your users to sign up for a Mailpoet newsletter list using a Fluent Forms
+- You can capture first name, last name, and (of course) email
+- You can signup users to as many lists as you like
+- You can set up the confirmation email via MailPoet
+- Ability to add mailpoet custom field
+
+== About Fluent Forms ==
+<a href="https://wordpress.org/plugins/fluentform">Fluent Forms</a> is a light-weight and fastest Form Builder plugin.
+
+Fluent Forms helps you create hassle-free contact forms, subscription forms, or any kind of forms you need for your website in minutes.
+
+==Amazing Fluent Forms Features==
+
+* Drag & Drop Contact Form Builder
+* Multi-column Form Layout
+* Smart Conditional Logic Form Fields
+* Conditional Confirmation Messages
+* Build 100% Responsive and Mobile Friendly Web Forms
+* Reusable Templates
+* Spam Protection with Google Recaptcha
+* Placeholder Customization
+* AJAX Submission and Both Client-Side and Server-Side Data Validation
+* Send Beautiful Email to submitter and admin
+* You can configure what will happen after form submission. You can show a success message, or you can redirect the user to a selected URL.
+* Restrict Your Web Form by user role
+* Show the form only for a time/date range.
+* 30+ Form Field Types to build any WebForms quickly.
+* In-Depth Analytics where you can see how many times it's displayed and how many leads were captured.
+* Set Default Value On Form Fields or you can populate from URL parameters
+* Export/Import Forms
+* MailChimp Integration - Add Lead/Contact on MailChimp on form submission
+* Slack Integration - Send Slack notification of Form Submission.
+* Export Data - You can export data as CSV/Exel/ODS or JSON format.
+* Advanced Data Filtering where you can see data by status and also by date range.
+
+
+[youtube https://www.youtube.com/watch?v=FFR2ZZaVF3g]
+
+
+== Installation ==
+This section describes how to install the plugin (Fluent Forms Connector for MailPoet) and get it working.
+Install From WordPress Admin Panel:
+
+1. Login to your WordPress Admin Area
+2. Go to Plugins -> Add New
+3. Type "Fluent Forms Connector for MailPoet" into the Search and hit Enter.
+4. Find this plugin Click "install now"
+5. Activate The Plugin
+6. Use MailPoet on your site form the FluentForms module dashboard.
+
+
+= Manual Installation =
+1. Download the "Fluent Forms Connector for MailPoet" plugin from WordPress.org repository
+2. On your WordPress admin dashboard, go to Plugins -> Add New -> Upload Plugin
+3. Upload the downloaded plugin file and click Install Now
+4. Be sure your FluentForms plugin is activated already.
+5. Activate "Fluent Forms Connector for MailPoet" from your Plugins page.
+6. Use Fluent Forms Connector for MailPoet on your site form the FluentForms module dashboard.
+
+== Screenshots ==
+
+
+== Changelog ==
+= 1.0.4 =
+* Fix custom field mapping
+
+= 1.0.3 =
+* WordPress 6.5.2 Support Added
+* Compatible with MailPoet v4.48.2
+
+= 1.0.0 =
+* WordPress 6.0 Support Added
+
+= 1.0.0 =
+* Init Release
+
